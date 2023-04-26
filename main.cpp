4ba79bc5b34883d93554a0f8c66428e877b91728
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-04-26 10:11:46 +0000

    Finalised

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+AnalogIn pot1(p19); //pot1 is an Object of class AnalogIn
+AnalogIn pot2(p20); //pot2 is an Object of class AnalogIn
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+PwmOut Red(p23);//PWM out for RGB Red 
+PwmOut Green(p24);//PWM out for RGB Green 
+int main(){
+        while(1){
+Red = pot1;//Red colour cotrolled by pot 1
+Green= pot2;//Green controlled by pot2 
+        printf("Red pot = %.2f \n\r",1- pot1.read());
+        printf("Green pot = %.2f \n\r",1- pot2.read());
+       wait(0.05);
+       }
 }
